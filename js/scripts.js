abb26343869b5bbb90c241f6430d0220789e4231
NathanRomike <nathanromike@gmail.com>
2015-10-24 01:30:24 -0700

    Pass second spec GREEN

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var beerSong = function (beers) {
-  return beers - 1;
+  if (beers === 0) {
+    return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+  } else {
+    return beers - 1;
+  }
 };
